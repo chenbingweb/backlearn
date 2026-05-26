d66afebec0e9bc861455313349c8c1a0ee006132
chenBing <chenbingweb@sina.com>
2026-05-26 15:18:22 +0800

    feat: 添加 Rust hello_rust 练习项目

---
+++ Rust/hello_rust/src/main.rs
@@ -0,0 +1,43 @@
+use chrono::Local;
+const MAX_AGE:u32= 1000_000;
+fn main() {
+    println!("Hello, world!");
+    println!("{},{}!","hello","Rust");
+    println!("{name}今年{age}岁",name="lili",age="23");
+    let now = Local::now();
+    println!("当前时间: {}", now.format("%Y-%m-%d %H:%M:%S"));
+    println!("最大年龄: {}", MAX_AGE);
+    let i:u8=0;
+    let c = i.checked_add(1);
+    println!("c: {:?}", c.unwrap());
+    println!("i: {}", i);
+    let t = true;
+    println!("t: {}", t);
+    if t {
+        println!("t is true");
+    }
+    else {
+        println!("t is false");
+    }
+
+    let tup:(i32,f64,&str) = (1,2.0,"hello");
+    println!("tup: {:?}", tup);
+    let (x,y,z) = tup;
+    println!("x: {}", x);
+    println!("y: {}", y);
+    println!("z: {}", z);
+    println!("z.len(): {}", z.len());
+    println!("z-: {}", tup.2);
+
+    let a = [1,2,3,4,5];
+    let b: [i32; 5] =[1,2,3,4,5];
+    let c =[3;4];
+
+    println!("a: {:?}", a);
+    println!("b: {:?}", b);
+    println!("c: {:?}", c);
+
+    let slice = &a[1..3];
+    println!("slice: {:?}", slice);
+    println!("slice.len(): {}", slice.len());
+}
